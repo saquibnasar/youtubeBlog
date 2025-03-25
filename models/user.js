75bd0862d872ff80edf85393612a41b4d9bff94c
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2025-03-25 23:41:08 +0530

    Add user model with password hashing, update home page to include navbar

---
+++ models/user.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require("mongoose");
+const { createHmac, randomBytes } = require("crypto");
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    salt: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileImageUrl: {
+      type: String,
+      default: "/public/images/defaultProfileImage.png",
+    },
+    role: {
+      type: String,
+      enum: ["ADMIN", "USER"],
+      default: "USER",
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return;
+
+  const salt = randomBytes(16).toString();
+
+  const hashPassword = createHmac("sha256", salt)
+    .update(user.password)
+    .digest("hex");
+
+  this.salt = salt;
+  this.password = hashPassword;
+
+  next();
+});
+
+module.exports = model("User", userSchema);
